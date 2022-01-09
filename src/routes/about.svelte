c5e2bb543277421eff5cd6b7bf0fb13997d70c9c
Maxi Ferreira <charca@gmail.com>
2022-01-08 21:49:39 -0800

    Static build adapter and about page

---
+++ src/routes/about.svelte
@@ -0,0 +1,3 @@
+<h1>About</h1>
+<p>This is the aboute page</p>
+<p>Back to <a href="/">index</a>.</p>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,3 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>Or visit the <a href="/about">about</a> page.</p>
